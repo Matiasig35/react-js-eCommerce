3ab363e25c0a1024013be19297eb1e3f5fb327b7
Matias Fernandez <matiasigfernandez@gmail.com>
2024-03-05 16:04:46 -0300

    Clase 2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Input } from './componets/Inputs';
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO WORLD FERNANDEZ</h1>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </div>
+  );
+}
+
+export default App
